1f9cdfa39be9a7e576f07e9f037e9300c97f09e5
kobpet <>
2019-04-10 20:34:05 +0200

    created better version of old FileHandler look in main for usage

--- data_tier_placeholder/datahandlers.py
+++ data_tier_placeholder/datahandlers.py
@@ -47,120 +47,59 @@ class BasicHandler(ABC):
 
     def __init__(self, path: str, query: str=None):
         self.query = query
-        self.path = path
+        self.path = Path(path)
 
     def get_list(self):
         """returns list of objects based on selected handler and query"""
         raise NotImplementedError
 
-@DeprecationWarning
+
 class FileHandler(BasicHandler):
-    # TODO: redo all
+    # TODO: add docs
     """
     Handler for file and folder type queries.
     Only allows generation of lists of images and single image objects
     Examples of usage:
-        getting an image list of corrected images:
 
-            image_list = FileHandler("folder_path/", "*.fits cdata").get_list()
-                - returns image_list of all fits files in specified folder
-
-        getting an image list of flat images:
-
-            image_list = FileHandler("flat_folder_path/", "*.fits flat")
-                - returns image_list of flat type image objects
-
-        getting a single image object:
-            image = FileHandler("file_path/image.fits", "data")
-
-            or
-
-            image = FileHandler("file_path/", "image.fits data")
-
-                -both return an Image type object of with specified path and data type
-
-    Allowed data types in query parameter:
-        flat - flat field images
-        dark - dark images
-        data - non-corrected (no dark current or flat field correction) images
-        ddata - dark corrected images
-        dfdata, cdata = dark and flat corrected data or otherwise corrected images used straight for photometry
-
-    Query format:
-        - query must always contain data type
-        - folder queries must always contain data type AND filter for selecting files in folder (minimum is
-            an asterisk *)
-        - queries for single file can be done either by specifiyng file in path and using only data type query or
-            specifying path of folder and then using query with filename and data type
-
-        Examples:
-            "*df.fits cdata"
-            "* cdata"
-            "flat"
-            "flat.fits flat"
     """
-# TODO: redo using pathlib
-    def __init__(self, path: str, query: str):
-        super().__init__(path, query)
-        squery = query.split(" ")
-        if len(squery) == 2:  # Query + data type
-            self.query = squery[0]
-            if any([data_type in squery[1] for data_type in ["flat", "dark", "data", "ddata", "cdata", "dfdata"]]):
-                self.type = squery[1]
-            else:  # data type is not in legal ones
-                raise InvalidQueryError("invalid data type")
-        else:  # Selecting single file, only data type necessary
-            if any([data_type in squery[0] for data_type in ["flat", "dark", "data", "ddata", "cdata", "dfdata"]]):
-                self.query = ""
-                self.type = squery[0]
-            else:
-                raise InvalidQueryError("invalid data type")
+    def __init__(self, folder: str, query=None, data_type="data"):
+        super().__init__(folder, query)
+        if data_type not in ["flat", "dark", "data", "cdata", "dfdata"]:
+            raise Exception("Invalid data type specified")
+
+        self.type = data_type
+        if self.query is None:
+            self.selector = "*.fits"
+        else:
+            self.selector = query  # TODO: CHECK FOR VALIDITY OF SELECTOR
 
     def get_list(self):
-        # TODO: Redo
-        gstring = self.path + self.query
-        # decide data type and corrections
+        # data type and corrections
         if self.type in ["flat", "dark", "data"]:
-            data_type = self.type
             processing_pars = {}
         elif self.type in ["cdata", "dfdata"]:
-            data_type = "data"
             processing_pars = {"flat": True, "dark": True}
+            self.type = "data"
         else:
-            data_type = "data"
             processing_pars = {"dark": True}
+            self.type = "data"
 
+        p = self.path
+        paths = list(p.glob(self.selector))
         image_list = []
-        k = 0
-        if len(glob.glob(gstring)) > 1:
-            for path in glob.glob(gstring):
-                image = pyfits.open(path)
+        for im_path in paths:
+                image = pyfits.open(im_path)
                 exposure = image[0].header["EXPOSURE"]
                 time_jd = image[0].header["JD"]
                 fixed_pars = {
                     "time_jd": time_jd,
-                    "path": Path(path),
-                    "type": data_type,
+                    "path": im_path,
+                    "type": self.type,
                     "exposure": exposure,
-                    "id": k}
+                    "id": im_path.stem}
                 image_list.append(Image(fixed_parameters=fixed_pars,
                                         processing_parameters=processing_pars))
-                k += 1
-        elif len(glob.glob(gstring)) == 0:
-            raise InvalidQueryError("no images fit query")
-        else:
-            image = pyfits.open(gstring)
-            exposure = image[0].header["EXPOSURE"]
-            time_jd = image[0].header["JD"]
-            fixed_pars = {
-                "time_jd": time_jd,
-                "path": gstring,
-                "type": data_type,
-                "exposure": exposure,
-                "id": k}
-            image_list = Image(fixed_parameters=fixed_pars,
-                               processing_parameters=processing_pars)
-            # TODO Decide whether to return list or single object if only one image matches query
+
         image_list.sort(key=lambda x: x.get_time_jd())
         return image_list
 
@@ -188,8 +127,10 @@ class FileHandlerDialog:
         return paths
 
     def get_list(self):
-        if self.type in ["ddata", "cdata", "dfdata"]:
+        if self.type in ["cdata", "dfdata"]:
             processing_parameters = {"flat": True, "dark": True}
+        elif self.type is "ddata":
+            processing_parameters = {"dark": True}
         else:
             processing_parameters = {}
         image_list = []
@@ -221,7 +162,8 @@ class DatabaseHandler(BasicHandler):
         super().__init__(path, query)
 
     def get_list(self):
-        engine = create_engine(self.path)
+        # TODO: WORKAROUND engine path sqlite:///
+        engine = create_engine("sqlite:///" + str(self.path))
         _session = sessionmaker()
         session = _session(bind=engine)
         image_list = self._load_images(session)
